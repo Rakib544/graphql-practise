4da7a31dfe6d5a73e427625bcb24af332d2bbce2
Rakib544 <md.rakib10122003@gmail.com>
2021-11-25 11:00:53 +0600

    graphql server created

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphql: true,
+  })
+);
+
+app.listen(8080);
