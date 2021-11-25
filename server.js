7588da80202f04fc17b14a7168692d045c1788cb
Rakib544 <md.rakib10122003@gmail.com>
2021-11-25 12:43:57 +0600

    server created successfull

--- server.js
+++ server.js
@@ -1,21 +1,80 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
-
-const schema = buildSchema(`
-  type Query {
-    message: String
-  }
-`);
-
-const root = { message: () => "Hello world!" };
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLSchema,
+} = require("graphql");
 
 const app = express();
+
+// creating fake data
+
+const Owners = [
+  { id: 1, name: "John Astle" },
+  { id: 2, name: "Gautam Sharma" },
+  { id: 3, name: "Kane Williamson" },
+];
+
+const Websites = [
+  { id: 1, name: "Facebook", ownerId: 1 },
+  { id: 2, name: "Google", ownerId: 2 },
+  { id: 3, name: "Amazon", ownerId: 3 },
+  { id: 4, name: "Github", ownerId: 1 },
+  { id: 5, name: "Medium", ownerId: 2 },
+  { id: 6, name: "Baidu", ownerId: 3 },
+  { id: 7, name: "Zapak", ownerId: 1 },
+  { id: 8, name: "Cricinfo", ownerId: 2 },
+];
+
+const websiteType = new GraphQLObjectType({
+  name: "Website",
+  description: "This represents a website made by a Owner(Programmer)",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    ownerId: { type: new GraphQLNonNull(GraphQLInt) },
+  }),
+});
+
+const ownerType = new GraphQLObjectType({
+  name: "Owner",
+  description: "This represents a Owner",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    websites: {
+      type: new GraphQLList(websiteType),
+      description: "List of all websites",
+      resolve: () => Owners,
+    },
+    owners: {
+      type: new GraphQLList(ownerType),
+      description: "List of all owners",
+      resolve: () => Owners,
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
+    // rootValue: root,
     graphiql: true,
   })
 );
