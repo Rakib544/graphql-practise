c8e1d9613dfe099eeaee2afac52d9207beaadd32
Rakib544 <md.rakib10122003@gmail.com>
2021-11-25 11:43:38 +0600

    server created and get request successful

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+  type Query {
+    message: String
+  }
+`);
+
+const root = { message: () => "Hello world!" };
+
 const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
-    graphql: true,
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
   })
 );
-
-app.listen(8080);
+app.listen(4000, () => console.log("Now browse to localhost:4000/graphql"));
